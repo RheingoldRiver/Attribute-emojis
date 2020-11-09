50fc8741e64e1305f860a571a0d512c27d0250f1
RheingoldRiver <18037011+RheingoldRiver@users.noreply.github.com>
2020-11-08 23:24:08 -0600

    omg it works

--- main.py
+++ main.py
@@ -1,49 +1,43 @@
 from PIL import Image
 from PIL import ImageDraw
-from PIL import ImageOps
 
-ELEMENTS = ['fire', 'water', 'wood', 'light', 'dark']
+ELEMENTS = ['fire', 'water', 'wood', 'light', 'dark', 'null']
 INPUT_FILE_PATTERN = 'originals/{}.png'
 OUTPUT_FILE_PATTERN = 'combined/{}_{}.png'
 
 DIM = 76
 
 
-def crop_image(img: Image, polygon):
+def make_polygon(path):
     """
-    Source: https://stackoverflow.com/questions/35620201/python-pil-make-pixels-outside-a-polygon-transparent
-    :param img: PIL image object
-    :param polygon: List of tuples
-    :return:
+    :param path:
+    :return: a mask to use to make the thing transparent
     """
-    back = Image.new('RGBA', img.size)
-    back.paste(img)
-    poly = Image.new('L', (512, 512))
+    poly = Image.new('RGBA', (DIM, DIM))
     pdraw = ImageDraw.Draw(poly)
-    pdraw.polygon(polygon, fill=255)
-    inverted_poly = ImageOps.invert(poly)
-    back.paste(poly, (0, 0), mask=inverted_poly)
-    back.show()
-    return back
+    pdraw.polygon(path, fill=(255, 255, 255, 255), outline=(255, 255, 255, 255))
+    return poly
 
 
 def run():
     for elem in ELEMENTS:
         upper_image = Image.open(INPUT_FILE_PATTERN.format(elem)).resize((DIM, DIM))
 
-        upper_polygon = [(0, 0), (0, DIM), (DIM, 0), (0, 0)]
-        upper_triangle = crop_image(upper_image, upper_polygon)
+        upper_path = [(DIM, DIM), (DIM, 0), (0, DIM), (DIM, DIM)]
+        upper_triangle = make_polygon(upper_path)
 
         for elem2 in ELEMENTS:
             if elem2 == elem:
                 continue
             lower_image = Image.open(INPUT_FILE_PATTERN.format(elem2)).resize((DIM, DIM))
 
-            lower_polygon = [(DIM, DIM), (DIM, 0), (0, DIM), (DIM, DIM)]
-            lower_triangle = crop_image(lower_image, lower_polygon)
+            lower_path = [(0, 0), (0, DIM), (DIM, 0), (0, 0)]
+            lower_triangle = make_polygon(lower_path)
 
-            upper_triangle.paste(lower_triangle, (0, 0), lower_triangle)
-            upper_triangle.save(OUTPUT_FILE_PATTERN.format(elem, elem2))
+            new = Image.new('RGBA', (DIM, DIM))
+            new.paste(upper_image, (0, 0), mask=lower_triangle)
+            new.paste(lower_image, (0, 0), mask=upper_triangle)
+            new.save(OUTPUT_FILE_PATTERN.format(elem, elem2))
 
 
 if __name__ == '__main__':
